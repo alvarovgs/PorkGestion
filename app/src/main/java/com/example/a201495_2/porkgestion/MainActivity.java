580cca9d2e64d7b84e722a72699ea5b23b74e660
Alvaro Vargas <alvarovgs@gmail.com>
2018-05-27 07:23:13 -0500

    Creación de menú principal Creación de página de inicio Se aagregan imágenes e iconos

--- app/src/main/java/com/example/a201495_2/porkgestion/MainActivity.java
+++ app/src/main/java/com/example/a201495_2/porkgestion/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.a201495_2.porkgestion;
 
 import android.os.Bundle;
+import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -22,10 +23,14 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "En construcción.....", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        }
+        );
+        startActivity(new Intent(this, MenuDrawerSimpleLight.class));
+
+
     }
 
     @Override
