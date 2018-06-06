001b849351551262289c4dd83a403844f5e3cdf9
FABIO <isaez.prisil@gmail.com>
2018-06-06 01:11:14 -0500

    Igual todo

---
+++ app/src/main/java/com/example/a201495_2/porkgestion/ventas.java
@@ -0,0 +1,20 @@
+package com.example.a201495_2.porkgestion;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ventas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ventas);
+    }
+
+    public void onClick(View view){
+        Intent miIntent=new Intent(ventas.this,MenuLateral.class);
+        startActivity(miIntent);
+    }
+}
