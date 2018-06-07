04ea18c86d3cb9541ea432972af71596ce222811
Natali <ozzyta1@gmail.com>
2018-06-07 00:04:38 -0500

    reproduccion

--- app/src/main/java/com/example/a201495_2/porkgestion/reproduccion.java
+++ app/src/main/java/com/example/a201495_2/porkgestion/reproduccion.java
@@ -5,16 +5,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import static com.pchmn.materialchips.R2.id.container;
+
 public class reproduccion extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_reproduccion);
+        setContentView(R.layout.activity_cerdo);
+
     }
 
+    //    codigo para pasar de activity con boton
     public void onClick(View view){
         Intent miIntent=new Intent(reproduccion.this,MenuLateral.class);
         startActivity(miIntent);
     }
+
+
 }
