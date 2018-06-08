f528ae282d14ac73c19b374e5362e9a3327c8d0e
ASMEVI <asmevi@hotmail.com>
2018-06-08 09:35:16 -0500

    actualiza

--- app/src/main/java/com/example/a201495_2/porkgestion/monta_Activity.java
+++ app/src/main/java/com/example/a201495_2/porkgestion/monta_Activity.java
@@ -1,13 +1,24 @@
 package com.example.a201495_2.porkgestion;
-
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class monta_Activity extends AppCompatActivity {
+
+    private Spinner sptipomonta1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monta);
+
+        sptipomonta1=(Spinner)findViewById(R.id.sp_tipomonta);
+        // decalaracion del vector unidimensional
+        String [] opcmonta = {"Monta Directa","Inseminación Artificial"};
+        //ArrayAdapter <String> adapter1 = new ArrayAdapter<String>(context: this, android.R.layout.simple_spinner_item,opcmonta);
+        //sptipomonta1.setAdapter(adapter1);
+
     }
 }
