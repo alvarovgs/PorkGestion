909ec92e3ced303c9eac3a66491a48c9f27449a7
ASMEVI <asmevi@hotmail.com>
2018-06-08 00:16:23 -0500

    Adision de funcionalidades Asuncion

--- app/src/main/java/com/example/a201495_2/porkgestion/pajilla_Activity.java
+++ app/src/main/java/com/example/a201495_2/porkgestion/pajilla_Activity.java
@@ -8,6 +8,6 @@ public class pajilla_Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_Pajilla_);
+        setContentView(R.layout.activity_pajilla);
     }
 }
