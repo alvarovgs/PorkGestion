e44256e887687c843696c1b565e4736faddf9116
ASMEVI <asmevi@hotmail.com>
2018-06-08 19:55:51 -0500

    actualiza

--- app/src/main/java/com/example/a201495_2/porkgestion/monta_Activity.java
+++ app/src/main/java/com/example/a201495_2/porkgestion/monta_Activity.java
@@ -1,8 +1,11 @@
 package com.example.a201495_2.porkgestion;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+
+
 
 public class monta_Activity extends AppCompatActivity {
 
@@ -14,11 +17,16 @@ public class monta_Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monta);
 
-        sptipomonta1=(Spinner)findViewById(R.id.sp_tipomonta);
-        // decalaracion del vector unidimensional
-        String [] opcmonta = {"Monta Directa","Inseminación Artificial"};
-        //ArrayAdapter <String> adapter1 = new ArrayAdapter<String>(context: this, android.R.layout.simple_spinner_item,opcmonta);
-        //sptipomonta1.setAdapter(adapter1);
-
+        sptipomonta1 = (Spinner) findViewById(R.id.sp_tipomonta);
+        //decalaracion del vector unidimensional
+        String[] opcmonta = {"Monta Directa", "Inseminación Artificial"};
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, opcmonta);
+        sptipomonta1.setAdapter(adapter1);
     }
-}
+        //public void selecionmonta=(View view){
+            //sptipomonta1.getSelectedItem().toString();
+            }
+
+
+
+
