89f7f46729e8397d3f6ccc7d31e93a1d63ef55f7
JenniferP88 <soprive@hotmail.es>
2018-06-08 19:44:49 -0500

    Cambio Logo, cambio color del hint en la pantalla inicial,creación de activity del segmento informativo con sus diversas opciones según estructura menú principal.

--- app/src/main/java/com/example/a201495_2/porkgestion/MainRegistrousuario.java
+++ app/src/main/java/com/example/a201495_2/porkgestion/MainRegistrousuario.java
@@ -1,13 +1,28 @@
 package com.example.a201495_2.porkgestion;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainRegistrousuario extends AppCompatActivity {
 
+    Button Inicio;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_registrousuario);
+
+        Inicio = (Button) findViewById(R.id.btn_reg);
+        Inicio.setOnClickListener (new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent Inicio = new Intent(MainRegistrousuario.this, MainActivity.class);
+                startActivity(Inicio);
+
+            }
+        });
     }
 }
