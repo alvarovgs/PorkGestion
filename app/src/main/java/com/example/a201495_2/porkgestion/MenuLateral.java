97a892a5ccf0d4724ab865fded89899a549bedb4
ASMEVI <asmevi@hotmail.com>
2018-06-09 23:03:38 -0500

    actualiza

--- app/src/main/java/com/example/a201495_2/porkgestion/MenuLateral.java
+++ app/src/main/java/com/example/a201495_2/porkgestion/MenuLateral.java
@@ -12,9 +12,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
-
 public class MenuLateral extends AppCompatActivity {
-
     private ActionBar actionBar;
     private Toolbar toolbar;
 
