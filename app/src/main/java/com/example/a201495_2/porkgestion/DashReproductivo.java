7e8efc30134436b99ed60ec4bd6f9b515051a9d6
ASMEVI <asmevi@hotmail.com>
2018-06-14 07:07:44 -0500

    actualiza

--- app/src/main/java/com/example/a201495_2/porkgestion/DashReproductivo.java
+++ app/src/main/java/com/example/a201495_2/porkgestion/DashReproductivo.java
@@ -7,40 +7,44 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
-
+import android.widget.Toast;
 public class DashReproductivo extends AppCompatActivity {
-ImageButton ibparto, ibmonta, ibpajilla;
+//ImageButton ibparto, ibmonta, ibpajilla;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dashreproductivo);
-        ibparto =(ImageButton) findViewById(R.id.ib_parto);
+        ImageButton ibparto =(ImageButton) findViewById(R.id.ib_parto);
         ibparto.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             Intent ibparto =new Intent(DashReproductivo.this, Parto_Activity.class);
             startActivity(ibparto);
+            Toast mostrar  = Toast.makeText(DashReproductivo.this, "Registo de Partos",Toast.LENGTH_LONG);
+            mostrar.show();
         }
         });
-        ibmonta=(ImageButton) findViewById(R.id.ib_monta);
+        ImageButton ibmonta=(ImageButton) findViewById(R.id.ib_monta);
         ibmonta.setOnClickListener(new View.OnClickListener() {
             @Override
         public void onClick(View v) {
             Intent ibmonta = new Intent(DashReproductivo.this, monta_Activity.class);
             startActivity(ibmonta);
-
-        }
+            Toast mostrar = Toast.makeText(DashReproductivo.this, "Regsito de Montas", Toast.LENGTH_LONG);
+            mostrar.show();        }
          });
 
-       ibpajilla =(ImageButton)findViewById(R.id.ib_pajilla);
-       ibpajilla.setOnClickListener(new View.OnClickListener() {
+        ImageButton ibpajilla =(ImageButton)findViewById(R.id.ib_pajilla);
+        ibpajilla.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View v) {
-               Intent ibpajilla =new Intent(DashReproductivo.this,pajilla_Activity.class);
+               Intent ibpajilla =new Intent(DashReproductivo.this, pajilla_Activity.class);
                startActivity(ibpajilla);
+               Toast mostarar =Toast.makeText(DashReproductivo.this, "Regstro de Pajillas", Toast.LENGTH_LONG);
+               mostarar.show();
+
            }
        });
-
 
     }
 }
