a19c7ef04743185ed3ebdbedc1fb6e57ef1bc50f
FABIO <isaez.prisil@gmail.com>
2018-06-15 02:17:13 -0500

    Navegacion
    Navegacion entre menu lateral, raza, ventas y reporte de ventas

--- app/src/main/java/com/example/a201495_2/porkgestion/reporteventa.java
+++ app/src/main/java/com/example/a201495_2/porkgestion/reporteventa.java
@@ -1,7 +1,9 @@
 package com.example.a201495_2.porkgestion;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class reporteventa extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class reporteventa extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reporteventa);
+    }
+    public void onClick(View view){
+        Intent miIntent=new Intent(reporteventa.this,reportes.class);
+        startActivity(miIntent);
     }
 }
