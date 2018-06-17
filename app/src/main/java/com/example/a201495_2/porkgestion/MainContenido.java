4b41da2ff87884a678506bb7b092f109d04d10b2
Alvaro Vargas <38003378+alvarovgs@users.noreply.github.com>
2018-06-17 15:02:33 -0500

    Resolver conflictros versión Jennifer

--- app/src/main/java/com/example/a201495_2/porkgestion/MainContenido.java
+++ app/src/main/java/com/example/a201495_2/porkgestion/MainContenido.java
@@ -4,34 +4,58 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+<<<<<<< HEAD
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
- public class MainContenido extends AppCompatActivity {
+public class MainContenido extends AppCompatActivity{
+
+    ImageView imgbutton;
+    TextView strigbutton;
+=======
+import android.widget.TextView;
+
+public class MainContenido extends AppCompatActivity {
     TextView tv_info;
-    ImageView informacion;
+>>>>>>> master
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contenido_inicio);
+<<<<<<< HEAD
+        imgbutton=(ImageView) findViewById(R.id.ibt_repro);
+        imgbutton.setOnClickListener(new View.OnClickListener() {
 
-     @Override
-      protected void onCreate(Bundle savedInstanceState) {
-          super.onCreate(savedInstanceState);
-          setContentView(R.layout.contenido_inicio);
-          tv_info=findViewById(R.id.tv_info);
-          informacion=findViewById(R.id.informacion);
+            @Override
+            public void onClick(View v) {
+                Intent imgbutton=new Intent(MainContenido.this, DashReproductivo.class);
+                startActivity(imgbutton);
 
-          tv_info.setOnClickListener(new View.OnClickListener() {
-              @Override
-              public void onClick(View v) {
-                  Intent IntentReg= new Intent (MainContenido.this, MainCiclo.class);
-                  MainContenido.this.startActivity(IntentReg);
-              }
-          });
+        strigbutton=(TextView)findViewById(R.id.repro_string);
+        strigbutton.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 Intent strigbutton = new Intent(MainContenido.this, DashReproductivo.class);
+                 startActivity(strigbutton);
 
-          informacion.setOnClickListener(new View.OnClickListener() {
-              @Override
-              public void onClick(View v) {
-                  Intent IntentReg= new Intent (MainContenido.this, MainCiclo.class);
-                  MainContenido.this.startActivity(IntentReg);
-              }
-          });
-      }
- }
+             }
+                });
+
+                }
+        });
+
+=======
+        tv_info=findViewById(R.id.tv_info);
+
+        tv_info.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent IntentReg = new Intent (MainContenido.this, MainInformacion.class);
+            startActivity(IntentReg);
+        }
+    });
+>>>>>>> master
+    }
+}
