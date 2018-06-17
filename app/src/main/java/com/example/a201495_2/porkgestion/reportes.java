4f635fbd08cb2e7a605cb030dc3d79760ff04e28
Alvaro Vargas <38003378+alvarovgs@users.noreply.github.com>
2018-06-17 15:07:46 -0500

    Resolver conflictos versión Jennifer

--- app/src/main/java/com/example/a201495_2/porkgestion/reportes.java
+++ app/src/main/java/com/example/a201495_2/porkgestion/reportes.java
@@ -28,9 +28,13 @@ public class reportes extends AppCompatActivity {
             case R.id.btn_verracos:
                 miIntent = new Intent(reportes.this, reporteverracos.class);
                 break;
-
+            
             case R.id.btn_ventas:
                 miIntent = new Intent(reportes. this, reporteventa.class);
+                break;
+                
+            case R.id.btn_hv:
+                miIntent = new Intent(reportes.this, MainReporteHV.class);
                 break;
         }
         startActivity(miIntent);
