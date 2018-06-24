41679aa0a033497aa165673152eab8be8571d6d2
ASMEVIES <Asmevi@hotmail.com>
2018-06-24 17:39:20 -0500

    actalizacion crud parto, monta

--- app/build/generated/source/r/debug/android/support/transition/R.java
+++ app/build/generated/source/r/debug/android/support/transition/R.java
@@ -64,53 +64,53 @@ public final class R {
         public static final int notify_panel_notification_icon_bg = 0x7f0800b2;
     }
     public static final class id {
-        public static final int action_container = 0x7f090010;
-        public static final int action_divider = 0x7f090012;
-        public static final int action_image = 0x7f090013;
-        public static final int action_text = 0x7f09001b;
-        public static final int actions = 0x7f09001c;
-        public static final int async = 0x7f090025;
-        public static final int blocking = 0x7f09002c;
-        public static final int chronometer = 0x7f09005e;
-        public static final int forever = 0x7f0900a5;
-        public static final int ghost_view = 0x7f0900a6;
-        public static final int icon = 0x7f0900b1;
-        public static final int icon_group = 0x7f0900b2;
-        public static final int info = 0x7f0900be;
-        public static final int italic = 0x7f0900c2;
-        public static final int line1 = 0x7f0900c9;
-        public static final int line3 = 0x7f0900ca;
-        public static final int normal = 0x7f090104;
-        public static final int notification_background = 0x7f090105;
-        public static final int notification_main_column = 0x7f090106;
-        public static final int notification_main_column_container = 0x7f090107;
-        public static final int parent_matrix = 0x7f09010d;
-        public static final int right_icon = 0x7f090121;
-        public static final int right_side = 0x7f090122;
-        public static final int save_image_matrix = 0x7f090126;
-        public static final int save_non_transition_alpha = 0x7f090127;
-        public static final int save_scale_type = 0x7f090128;
-        public static final int tag_transition_group = 0x7f090155;
-        public static final int text = 0x7f090157;
-        public static final int text2 = 0x7f090158;
-        public static final int time = 0x7f09017d;
-        public static final int title = 0x7f09017e;
-        public static final int transition_current_scene = 0x7f090185;
-        public static final int transition_layout_save = 0x7f090186;
-        public static final int transition_position = 0x7f090187;
-        public static final int transition_scene_layoutid_cache = 0x7f090188;
-        public static final int transition_transform = 0x7f090189;
+        public static final int action_container = 0x7f090011;
+        public static final int action_divider = 0x7f090013;
+        public static final int action_image = 0x7f090014;
+        public static final int action_text = 0x7f09001c;
+        public static final int actions = 0x7f09001d;
+        public static final int async = 0x7f090026;
+        public static final int blocking = 0x7f09002d;
+        public static final int chronometer = 0x7f090062;
+        public static final int forever = 0x7f0900af;
+        public static final int ghost_view = 0x7f0900b0;
+        public static final int icon = 0x7f0900bb;
+        public static final int icon_group = 0x7f0900bc;
+        public static final int info = 0x7f0900c8;
+        public static final int italic = 0x7f0900cc;
+        public static final int line1 = 0x7f0900d7;
+        public static final int line3 = 0x7f0900d8;
+        public static final int normal = 0x7f090112;
+        public static final int notification_background = 0x7f090113;
+        public static final int notification_main_column = 0x7f090114;
+        public static final int notification_main_column_container = 0x7f090115;
+        public static final int parent_matrix = 0x7f09011b;
+        public static final int right_icon = 0x7f09012e;
+        public static final int right_side = 0x7f09012f;
+        public static final int save_image_matrix = 0x7f090133;
+        public static final int save_non_transition_alpha = 0x7f090134;
+        public static final int save_scale_type = 0x7f090135;
+        public static final int tag_transition_group = 0x7f090163;
+        public static final int text = 0x7f090165;
+        public static final int text2 = 0x7f090166;
+        public static final int time = 0x7f09018d;
+        public static final int title = 0x7f09018e;
+        public static final int transition_current_scene = 0x7f090195;
+        public static final int transition_layout_save = 0x7f090196;
+        public static final int transition_position = 0x7f090197;
+        public static final int transition_scene_layoutid_cache = 0x7f090198;
+        public static final int transition_transform = 0x7f090199;
     }
     public static final class integer {
         public static final int status_bar_notification_info_maxnum = 0x7f0a000a;
     }
     public static final class layout {
-        public static final int notification_action = 0x7f0b0057;
-        public static final int notification_action_tombstone = 0x7f0b0058;
-        public static final int notification_template_custom_big = 0x7f0b005f;
-        public static final int notification_template_icon_group = 0x7f0b0060;
-        public static final int notification_template_part_chronometer = 0x7f0b0064;
-        public static final int notification_template_part_time = 0x7f0b0065;
+        public static final int notification_action = 0x7f0b0058;
+        public static final int notification_action_tombstone = 0x7f0b0059;
+        public static final int notification_template_custom_big = 0x7f0b0060;
+        public static final int notification_template_icon_group = 0x7f0b0061;
+        public static final int notification_template_part_chronometer = 0x7f0b0065;
+        public static final int notification_template_part_time = 0x7f0b0066;
     }
     public static final class string {
         public static final int status_bar_notification_info_overflow = 0x7f0e008a;
--- app/src/main/java/com/example/a201495_2/porkgestion/Parto_Activity.java
+++ app/src/main/java/com/example/a201495_2/porkgestion/Parto_Activity.java
@@ -1,16 +1,126 @@
 package com.example.a201495_2.porkgestion;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.example.a201495_2.porkgestion.AdminSQLiteOpenHelper;
 
 public class Parto_Activity extends AppCompatActivity {
+    private EditText codigoparto,numerocerda, nombrecerda, fechaparto, totallechones, hembrasvivas, hembrasmuertas,
+    machosvivos, machosmuertos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parto_);
+        codigoparto=(EditText)findViewById(R.id.ed_codigparto);
+        numerocerda=(EditText)findViewById(R.id.ed_numerocerda);
+        nombrecerda=(EditText)findViewById(R.id.ed_nombrecerda);
+        fechaparto=(EditText)findViewById(R.id.ed_fechaparto);
+        totallechones=(EditText)findViewById(R.id.ed_totallechones);
+        hembrasvivas=(EditText)findViewById(R.id.ed_hembrasvivas);
+        hembrasmuertas=(EditText)findViewById(R.id.ed_hembrasmuertas);
+        machosvivos=(EditText)findViewById(R.id.ed_machosvivos);
+        machosmuertos=(EditText)findViewById(R.id.ed_machosmuertos);
+
+    }
+
+    public void ingresar(View view){
+        AdminSQLiteOpenHelper admin =new AdminSQLiteOpenHelper(this, "reproduccion", null, 1);
+        SQLiteDatabase BaseDatos=admin.getWritableDatabase();
+
+        String Codigoparto = codigoparto.getText().toString();
+        String Numerocerda = numerocerda.getText().toString();
+        String Nombrecerda = nombrecerda.getText().toString();
+        String Fechaparto= fechaparto.getText().toString();
+        String Totallechones = totallechones.getText().toString();
+        String Hembrasvivas = hembrasvivas.getText().toString();
+        String Hembrasmuertas = hembrasmuertas.getText().toString();
+        String Machosvivos = machosvivos.getText().toString();
+        String Machosmuertos = machosmuertos.getText().toString();
+
+        if(!Codigoparto.isEmpty()&&!Numerocerda.isEmpty() &&!Nombrecerda.isEmpty()
+                &&!Fechaparto.isEmpty()&&!Totallechones.isEmpty()){
+            ContentValues regsitro=new ContentValues();
+            regsitro.put("campo_codigoparto", Codigoparto);
+            regsitro.put("campo_numerocerda", Numerocerda);
+            regsitro.put("campo_nombrecerda", Nombrecerda);
+            regsitro.put("campo_fechaparto", Fechaparto);
+            regsitro.put("campo_hembrasvivas", Hembrasvivas);
+            regsitro.put("campo_hembrasmuertas", Hembrasmuertas);
+            regsitro.put("campo_machosvivos", Machosvivos);
+            regsitro.put("campo_machosmuertos", Machosmuertos);
+
+            BaseDatos.insert("parto", null, regsitro);
+            BaseDatos.close();
+            codigoparto.setText("");
+            numerocerda.setText("");
+            nombrecerda.setText("");
+            fechaparto.setText("");
+            totallechones.setText("");
+            hembrasvivas.setText("");
+            hembrasmuertas.setText("");
+            machosvivos.setText("");
+            machosmuertos.setText("");
+
+            Toast.makeText(this,"Se ha Ingresado un regsitro de Parto",Toast.LENGTH_SHORT).show();
+
+        }else{
+            Toast.makeText(this, "Debes ingresar Los Datos",Toast.LENGTH_LONG).show();
+
+        }
+
+    }
+
+    //Método para consultar un Regsitro de Parto
+    public void Consultar(View view){
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "reproduccion", null, 1);
+        SQLiteDatabase BaseDatos = admin.getWritableDatabase();
+        String Codigoparto = codigoparto.getText().toString();
+
+        if(!Codigoparto.isEmpty()){
+            Cursor fila = BaseDatos.rawQuery
+                    ("select campo_numerocerda, campo_nombrecerda, campo_fechaparto, campo_totallechones," +
+                            " campo_hembrasvivas, campo_hembrasmuertas, campo_machosvivos,campo_machosmuertos" +
+                            " from parto where campo_codigoparto =" + Codigoparto, null);
+
+            if(fila.moveToFirst()){
+                numerocerda.setText(fila.getString(0));
+                nombrecerda.setText(fila.getString(1));
+                fechaparto.setText(fila.getString(2));
+                totallechones.setText(fila.getString(3));
+                hembrasvivas.setText(fila.getString(4));
+                hembrasmuertas.setText(fila.getString(5));
+                machosvivos.setText(fila.getString(6));
+                machosmuertos.setText(fila.getString(7));
+                BaseDatos.close();
+            } else {
+                Toast.makeText(this,"No existe Registro de parto con ese código", Toast.LENGTH_SHORT).show();
+                BaseDatos.close();
+            }
+
+        } else {
+            Toast.makeText(this, "Debes introducir el codigo de Parto a Consultar", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void Limpiar(View view){
+        codigoparto.setText("");
+        numerocerda.setText("");
+        nombrecerda.setText("");
+        fechaparto.setText("");
+        totallechones.setText("");
+        hembrasvivas.setText("");
+        hembrasmuertas.setText("");
+        machosvivos.setText("");
+        machosmuertos.setText("");
+        codigoparto.requestFocus();
     }
 
     public void onClick(View view){
