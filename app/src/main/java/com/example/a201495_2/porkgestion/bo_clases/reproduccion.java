f5872c25bdc48738821d5e578f74429421afca0b
Natali Velasquez <38143334+OZZYTA@users.noreply.github.com>
2018-06-27 23:52:31 -0500

    Add files via upload

--- app/src/main/java/com/example/a201495_2/porkgestion/bo_clases/reproduccion.java
+++ app/src/main/java/com/example/a201495_2/porkgestion/bo_clases/reproduccion.java
@@ -8,21 +8,24 @@ import java.util.ArrayList;
 
 public class reproduccion {
     private int idHembra, idVerraco, idPajilla;
-    private String strFechaCelo, strError;
+    private String strError;
     private String strFechaMonta;
     private String strTipoMonta;
+    private String strEstado;
     private Context appContext;
     public boolean expanded = false;
     private dataBaseOpenHelper dbAcces;
 
-    public reproduccion(Context appContext, int idCerdo, String strFechaCelo, String strFechaMonta, String strTipoMonta, int numeroVerraco, int numeroPajilla) {
+
+
+    public reproduccion(Context appContext, int idHembra, String strEstado, String strFechaMonta, String strTipoMonta, int idVerraco, int idPajilla) {
         this.appContext = appContext;
-        this.idHembra = idCerdo;
-        this.idVerraco = numeroVerraco;
-        this.idPajilla = numeroPajilla;
-        this.strFechaCelo = strFechaCelo;
+        this.idHembra = idHembra;
+        this.idVerraco = idVerraco;
+        this.idPajilla = idPajilla;
         this.strFechaMonta = strFechaMonta;
         this.strTipoMonta = strTipoMonta;
+        this.strEstado = strEstado;
     }
 
     public reproduccion(Context appContext){
@@ -39,10 +42,6 @@ public class reproduccion {
 
     public void setIdPajilla(int idPajilla) {
         this.idPajilla = idPajilla;
-    }
-
-    public void setStrFechaCelo(String strFechaCelo) {
-        this.strFechaCelo = strFechaCelo;
     }
 
     public void setStrFechaMonta(String strFechaMonta) {
@@ -65,10 +64,6 @@ public class reproduccion {
         return idPajilla;
     }
 
-    public String getStrFechaCelo() {
-        return strFechaCelo;
-    }
-
     public String getStrFechaMonta() {
         return strFechaMonta;
     }
@@ -81,123 +76,135 @@ public class reproduccion {
         return strError;
     }
 
-    public boolean insertMonta(){
+    public String getStrEstado() {
+        return strEstado;
+    }
+
+    public void setStrEstado(String strEstado) {
+        this.strEstado = strEstado;
+    }
+
+
+    public boolean regprenez(){
         dbAcces = new dataBaseOpenHelper(appContext);
         ContentValues ctValores = new ContentValues();
-        ctValores.put("FECHACELO",this.strFechaCelo);
-        ctValores.put("FECHAMONTA",this.strFechaMonta);
-        ctValores.put("TIPOMONTA",this.strTipoMonta);
+        ctValores.put("TIPO",this.strTipoMonta);
         ctValores.put("IDHEMBRA",this.idHembra);
         ctValores.put("IDVERRACO",this.idVerraco);
         ctValores.put("IDPAJILLA",this.idPajilla);
+        ctValores.put("FECHA",this.strFechaMonta);
+        ctValores.put("ESTADO",this.strEstado);
         dbAcces.insertDatabase("REPRODUCCION",ctValores);
         this.strError = dbAcces.getErrorDB();
         return dbAcces.getErrorDB()==null;
     }
 
-    public Boolean updateMonta(){
+    public Boolean updateprenez(){
         dbAcces = new dataBaseOpenHelper(appContext);
         ContentValues ctValores = new ContentValues();
-        ctValores.put("FECHACELO",this.strFechaCelo);
-        ctValores.put("FECHAMONTA",this.strFechaMonta);
-        ctValores.put("TIPOMONTA",this.strTipoMonta);
+        ctValores.put("TIPO",this.strTipoMonta);
         ctValores.put("IDHEMBRA",this.idHembra);
         ctValores.put("IDVERRACO",this.idVerraco);
         ctValores.put("IDPAJILLA",this.idPajilla);
+        ctValores.put("FECHA",this.strFechaMonta);
+        ctValores.put("ESTADO",this.strEstado);
         String strArgs[] = new String[]{String.valueOf(this.idHembra)};
         dbAcces.updateDatabase("REPRODUCCION",ctValores,"IDHEMBRA=?",strArgs);
         this.strError = dbAcces.getErrorDB();
         return dbAcces.getErrorDB()==null;
     }
 
-    public Boolean deleteMonta(){
+    public Boolean deleteprenez(){
         dbAcces = new dataBaseOpenHelper(appContext);
         String strArgs[] = new String[]{String.valueOf(this.idHembra)};
         dbAcces.deleteDatabase ("REPRODUCCION","IDHEMBRA=?",strArgs);
         return dbAcces.getErrorDB()==null;
     }
 
-    public reproduccion getMontaByView(String strCodigo){
+    public reproduccion getPrenezByView(String strIdCerda){
         dbAcces = new dataBaseOpenHelper(appContext);
-        String strColumns[] = new String[]{"IDHEMBRA","IDVERRACO","IDPAJILLA","FECHACELO","FECHAMONTA","TIPOMONTA"};
+        String strColumns[] = new String[]{"TIPO","IDHEMBRA","IDVERRACO","IDPAJILLA","FECHA","ESTADO"};
         reproduccion tmpObject = new reproduccion(appContext);
+        String strArgs[] = new String[]{strIdCerda};
+
         Cursor crResult;
         dbAcces.openDataBase();
-        crResult = dbAcces.qweryDatabase("VW_REPRODUCCION", strColumns, "(IDHEMBRA=?)", null, null);
+        crResult = dbAcces.qweryDatabase("REPRODUCCION", strColumns, "(IDHEMBRA=?)",strArgs, null);
         if (crResult.moveToFirst()) {
-            tmpObject.setIdHembra(crResult.getInt(0));
-            tmpObject.setIdVerraco(crResult.getInt(1));
-            tmpObject.setIdPajilla(crResult.getInt(2));
-            tmpObject.setStrFechaCelo(crResult.getString(3));
+            tmpObject.setStrTipoMonta(crResult.getString(0));
+            tmpObject.setIdHembra(crResult.getInt(1));
+            tmpObject.setIdVerraco(crResult.getInt(2));
+            tmpObject.setIdPajilla(crResult.getInt(3));
             tmpObject.setStrFechaMonta(crResult.getString(4));
-            tmpObject.setStrTipoMonta(crResult.getString(5));
+            tmpObject.setStrEstado(crResult.getString(5));
         }
         this.strError = dbAcces.getErrorDB();
         dbAcces.closeDataBase();
         return tmpObject;
     }
 
-    public reproduccion getreproduccionByTable(String strCodigo){
+    public reproduccion getPrenezByTable(String stridhembra){
         dbAcces = new dataBaseOpenHelper(appContext);
-        String strColumns[] = new String[]{"IDHEMBRA","IDVERRACO","IDPAJILLA","FECHA","TIPO"};
+        String strColumns[] = new String[]{"TIPO","IDHEMBRA","IDVERRACO","IDPAJILLA","FECHA","ESTADO"};
         reproduccion tmpObject = new reproduccion(appContext);
-        String strArgs[] = new String[]{strCodigo};
+        String strArgs[] = new String[]{stridhembra};
         Cursor crResult;
         dbAcces.openDataBase();
-        crResult = dbAcces.qweryDatabase("REPRODUCCION", strColumns, "(IDHEMBRA=?)" , strArgs, null);
+        crResult = dbAcces.qweryDatabase("REPRODUCCION", strColumns, "(IDHEMBRA=?)",strArgs, null);
         if (crResult.moveToFirst()) {
-            tmpObject.setIdHembra(crResult.getInt(0));
-            tmpObject.setIdVerraco(crResult.getInt(1));
-            tmpObject.setIdPajilla(crResult.getInt(2));
-            tmpObject.setStrFechaMonta(crResult.getString(3));
-            tmpObject.setStrTipoMonta(crResult.getString(4));
+            tmpObject.setStrTipoMonta(crResult.getString(0));
+            tmpObject.setIdHembra(crResult.getInt(1));
+            tmpObject.setIdVerraco(crResult.getInt(2));
+            tmpObject.setIdPajilla(crResult.getInt(3));
+            tmpObject.setStrFechaMonta(crResult.getString(4));
+            tmpObject.setStrEstado(crResult.getString(5));
         }
         this.strError = dbAcces.getErrorDB();
         dbAcces.closeDataBase();
         return tmpObject;
     }
 
-    public ArrayList<reproduccion> getAllreproduccionByView(){
+    public ArrayList<reproduccion> getAllPrenezByView(){
         dbAcces = new dataBaseOpenHelper(appContext);
-        String strColumns[] = new String[]{"IDHEMBRA","IDVERRACO","IDPAJILLA","FECHACELO","FECHAMONTA","TIPOMONTA"};
+        String strColumns[] = new String[]{"TIPO","IDHEMBRA","IDVERRACO","IDPAJILLA","FECHA","ESTADO"};
         Cursor crResult;
-        ArrayList<reproduccion> listMonta = new ArrayList<>();
+        ArrayList<reproduccion> listprenez = new ArrayList<>();
         dbAcces.openDataBase();
-        crResult = dbAcces.qweryDatabase("VW_REPRODUCCION", strColumns, null, null, null);
+        crResult = dbAcces.qweryDatabase("VW_REPRODUCCION", strColumns, "(IDHEMBRA=?",null,null);
         if (crResult.moveToFirst()) {
             do {
                 reproduccion tmpObject = new reproduccion(appContext);
-                tmpObject.setIdHembra(crResult.getInt(0));
-                tmpObject.setIdVerraco(crResult.getInt(1));
-                tmpObject.setIdPajilla(crResult.getInt(2));
-                tmpObject.setStrFechaCelo(crResult.getString(3));
+                tmpObject.setStrTipoMonta(crResult.getString(0));
+                tmpObject.setIdHembra(crResult.getInt(1));
+                tmpObject.setIdVerraco(crResult.getInt(2));
+                tmpObject.setIdPajilla(crResult.getInt(3));
                 tmpObject.setStrFechaMonta(crResult.getString(4));
-                tmpObject.setStrTipoMonta(crResult.getString(5));
-                listMonta.add(tmpObject);
+                tmpObject.setStrEstado(crResult.getString(5));
+                listprenez.add(tmpObject);
             } while (crResult.moveToNext());
 
         }
         this.strError = dbAcces.getErrorDB();
         dbAcces.closeDataBase();
-        return listMonta;
+        return listprenez;
     }
 
-    public ArrayList<reproduccion> getAllreproduccionByTable(){
+    public ArrayList<reproduccion> getAllPrenezByTable(){
         dbAcces = new dataBaseOpenHelper(appContext);
-        String strColumns[] = new String[]{"IDHEMBRA","IDVERRACO","IDPAJILLA","FECHACELO","FECHAMONTA","TIPOMONTA"};
+        String strColumns[] = new String[]{"TIPO","IDHEMBRA","IDVERRACO","IDPAJILLA","FECHA","ESTADO"};
         Cursor crResult;
         ArrayList<reproduccion> listObject = new ArrayList<reproduccion>();
         dbAcces.openDataBase();
-        crResult = dbAcces.qweryDatabase("REPRODUCCION", strColumns, null, null, null);
+        crResult = dbAcces.qweryDatabase("REPRODUCCION", strColumns, null,null, null);
         if (crResult.moveToFirst()) {
             do {
                 reproduccion tmpObject = new reproduccion(appContext);
-                tmpObject.setIdHembra(crResult.getInt(0));
-                tmpObject.setIdVerraco(crResult.getInt(1));
-                tmpObject.setIdPajilla(crResult.getInt(2));
-                tmpObject.setStrFechaCelo(crResult.getString(3));
+                tmpObject.setStrTipoMonta(crResult.getString(0));
+                tmpObject.setIdHembra(crResult.getInt(1));
+                tmpObject.setIdVerraco(crResult.getInt(2));
+                tmpObject.setIdPajilla(crResult.getInt(3));
                 tmpObject.setStrFechaMonta(crResult.getString(4));
-                tmpObject.setStrTipoMonta(crResult.getString(5));
+                tmpObject.setStrEstado(crResult.getString(5));
                 listObject.add(tmpObject);
             } while (crResult.moveToNext());
         }
@@ -206,7 +213,7 @@ public class reproduccion {
         return listObject;
     }
 
-    public ArrayList<reproduccion>  getCerdoByQwery(String strvalor1, String strValor2, String strVaslor3){
+    public ArrayList<reproduccion>  getPrenezByQwery(String strvalor1, String strValor2, String strVaslor3){
         dbAcces = new dataBaseOpenHelper(appContext);
         Cursor crResult;
         String strSql=null;
@@ -218,13 +225,12 @@ public class reproduccion {
         if (crResult.moveToFirst()) {
             do {
                 reproduccion tmpObject = new reproduccion(appContext);
-                tmpObject.setIdHembra(crResult.getInt(0));
-                tmpObject.setIdVerraco(crResult.getInt(1));
-                tmpObject.setIdPajilla(crResult.getInt(2));
-                tmpObject.setStrFechaCelo(crResult.getString(3));
+                tmpObject.setStrTipoMonta(crResult.getString(0));
+                tmpObject.setIdHembra(crResult.getInt(1));
+                tmpObject.setIdVerraco(crResult.getInt(2));
+                tmpObject.setIdPajilla(crResult.getInt(3));
                 tmpObject.setStrFechaMonta(crResult.getString(4));
-                tmpObject.setStrTipoMonta(crResult.getString(5));
-                listreproduccion.add(tmpObject);
+                tmpObject.setStrEstado(crResult.getString(5));
             } while (crResult.moveToNext());
         }
 
@@ -243,18 +249,5 @@ public class reproduccion {
         return dbAcces.getErrorDB()==null;
     }
 
-    public Boolean existReproduccion(int idCerdo){
-        dbAcces = new dataBaseOpenHelper(appContext);
-        Boolean bResult  = false;
-        Cursor crResult;
-        String strSql = String.format("SELECT COUNT(*) AS TOTAL FROM REPRODUCCION WHERE IDREPRODUCCION='%s'",idCerdo);
-        dbAcces.openDataBase();
-        crResult = dbAcces.qweryDatabaseBySql(strSql);
-        if (crResult.moveToFirst()) {
-            bResult  =  crResult.getInt(0)>0;
-        }
-        dbAcces.closeDataBase();
-        return bResult;
-    }
 
 }
