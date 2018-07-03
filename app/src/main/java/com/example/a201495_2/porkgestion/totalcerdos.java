d00348003aaa61ceec99ead0d719aa1823578d15
Alvaro Vargas <38003378+alvarovgs@users.noreply.github.com>
2018-07-02 21:22:22 -0500

    Update totalcerdos.java

--- app/src/main/java/com/example/a201495_2/porkgestion/totalcerdos.java
+++ app/src/main/java/com/example/a201495_2/porkgestion/totalcerdos.java
@@ -1,13 +1,20 @@
 package com.example.a201495_2.porkgestion;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 import com.example.a201495_2.porkgestion.bo_clases.Cerdo;
+
+
 
 
 public class totalcerdos extends AppCompatActivity {
@@ -15,19 +22,19 @@ public class totalcerdos extends AppCompatActivity {
     ListView listViewCerdos;
     ArrayList<String> listaInformacion;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_totalcerdos);
 
-        listViewCerdos= findViewById(R.id.listViewCerdos);
+
+        listViewCerdos = (ListView) findViewById(R.id.listViewCerdos);
         Cerdo lista = new Cerdo(getApplicationContext());
         listaInformacion = lista.consultaCerdo();
-       // consultarListaPersonas();
+        // consultarListaPersonas();
 
-        ArrayAdapter adaptador=new ArrayAdapter(this,android.R.layout.simple_spinner_item,listaInformacion);
+        ArrayAdapter adaptador = new ArrayAdapter(this, android.R.layout.simple_spinner_item, listaInformacion);
         listViewCerdos.setAdapter(adaptador);
-
     }
-
 }
